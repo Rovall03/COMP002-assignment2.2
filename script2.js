f4a3d6557b6c2de665bd2b54638cb455a9b60be7
rosa ovalle <you@example.com>
2024-10-27 21:36:18 -0700

    fizz part2 added

--- script2.js
+++ script2.js
@@ -9,3 +9,7 @@
 // 14
 // FizzBuzz
 // ...
+for (let n = 1 ; n <= 100; n++) {let output = "";
+    if (n % 3 == 0) output += "Fizz";
+    if (n % 5 == 0) output += "buzz";
+    console.log( output || n);}
